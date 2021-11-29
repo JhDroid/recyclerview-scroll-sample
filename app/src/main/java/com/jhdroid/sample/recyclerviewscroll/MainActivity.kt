ade2e124157d1800a9db2cf4e6e009e052374f06
JhDroid <lorzx77@gmail.com>
2021-11-29 22:27:48 +0900

    버그 수정

--- app/src/main/java/com/jhdroid/sample/recyclerviewscroll/MainActivity.kt
+++ app/src/main/java/com/jhdroid/sample/recyclerviewscroll/MainActivity.kt
@@ -32,8 +32,9 @@ class MainActivity : AppCompatActivity() {
             val rect = Rect()
             binding.clRootContainer.getWindowVisibleDisplayFrame(rect)
 
-            val heightDiff = binding.clRootContainer.height - (rect.bottom - rect.top)
-            isOpen = heightDiff > 100 // true == 키보드 올라감
+            val rootViewHeight = binding.clRootContainer.rootView.height
+            val heightDiff = rootViewHeight - rect.height()
+            isOpen = heightDiff > rootViewHeight * 0.25 // true == 키보드 올라감
         }
     }
 
@@ -45,16 +46,20 @@ class MainActivity : AppCompatActivity() {
             addItemDecoration(SpaceDecoration())
 
             /**
-             * 최초 진입 등 데이터가 얼마 없는 경우, StackFromEnd 설정 전까지 해당 Listener에서 스크롤 처리
+             * 1. 키보드가 올라온 경우에만 스크롤이 가능한 경우 처리
+             * 키보드가 내려간 경우 스크롤이 불가능하지만 키보드가 올라오면서 스크롤이 가능한 경우
              * */
             addOnLayoutChangeListener(onLayoutChangeListener)
 
             /**
+             * 2. 키보드가 올라온 상태에서 데이터를 추가해 키보드가 내려갔을 때에도 스크롤이 가능한 경우
+             * 3. 화면 진입 시 데이터를 불러와 청므부터 스크롤이 가능한 경우
              * 키보드가 열리지 않은 상태에서 스크롤 가능 상태이면 StackFromEnd 설정
              * 키보드가 열린 상태에서 체크하면 키보드가 사라질 때 목록이 하단에 붙을 수 있음
              * */
             viewTreeObserver.addOnScrollChangedListener {
                 if (isScrollable() && !isOpen) {
+                    Log.d("jhkimTest", "isOpen :: $isOpen")
                     setStackFromEnd()
                     removeOnLayoutChangeListener(onLayoutChangeListener)
                 }
@@ -74,8 +79,7 @@ class MainActivity : AppCompatActivity() {
 
     private val onLayoutChangeListener =
         View.OnLayoutChangeListener { _, _, _, _, bottom, _, _, _, oldBottom ->
-            // 키보드가 올라와 높이가 변함, StackFromEnd 설정되어있으면 동작하지 않음
-//            if (bottom < oldBottom && !binding.rvDataList.getStackFromEnd()) {
+            // 키보드가 올라와 높이가 변함
             if (bottom < oldBottom) {
                 binding.rvDataList.scrollBy(0, oldBottom - bottom) // 스크롤 유지를 위해 추가
             }
